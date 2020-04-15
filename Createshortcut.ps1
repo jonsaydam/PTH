a6437b7e9da02e4fc377d311f74c44b2a29fe418
Jon <saydamcan@gmail.com>
2020-04-15 09:10:10 -0400

    First

---
+++ Createshortcut.ps1
@@ -0,0 +1,8 @@
+﻿$SourceFileLocation = "C:\users\Kerry\Desktop\PTH\pokerth.exe"
+$ShortcutLocation = “C:\Users\Kerry\Desktop\poker.lnk”
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+$Shortcut.TargetPath = $SourceFileLocation
+$Shortcut.Save()
